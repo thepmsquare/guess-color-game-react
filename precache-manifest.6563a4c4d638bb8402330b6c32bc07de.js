8c92756dba49e256bb58e003e600fa096baa1b2d
thePmSquare <impmmmm@gmail.com>
2020-09-06 21:26:58 +0530

    Updates

---
+++ precache-manifest.6563a4c4d638bb8402330b6c32bc07de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15380ebe899075ef6488179ef4f1296",
+    "url": "/guess-color-game-react/index.html"
+  },
+  {
+    "revision": "63b180e5871f2668e76a",
+    "url": "/guess-color-game-react/static/css/main.2d1558da.chunk.css"
+  },
+  {
+    "revision": "064dab91e20f86838a9b",
+    "url": "/guess-color-game-react/static/js/2.af6aec83.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/guess-color-game-react/static/js/2.af6aec83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b180e5871f2668e76a",
+    "url": "/guess-color-game-react/static/js/main.c62fadae.chunk.js"
+  },
+  {
+    "revision": "b7e93bb4f5165569c794",
+    "url": "/guess-color-game-react/static/js/runtime-main.d548d86c.js"
+  }
+]);
